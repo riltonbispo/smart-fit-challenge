54b71fbae1423a59876e51a44099ce302ea1b555
rilton bispo <riltondev@gmail.com>
2023-09-28 17:08:27 -0300

    feat: add function request api

---
+++ src/services/gymApi.ts
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+import { GymType } from '@/types/gymType'
+
+export const getGym = async () => {
+  try {
+    const response = await axios.get(
+      'https://test-frontend-developer.s3.amazonaws.com/data/locations.json',
+    )
+    return response.data.locations
+  } catch (err) {
+    console.error('ERRO!', err)
+    throw err
+  }
+}
+
+export const selectGym = async (selectTurn: string) => {
+  try {
+    const response = await axios.get(
+      'https://test-frontend-developer.s3.amazonaws.com/data/locations.json',
+    )
+
+    const gyms: GymType[] = response.data.locations
+
+    // Filtrar apenas as academias que têm schedules definidos
+    const filteredGyms = gyms.filter((gym) => gym.schedules)
+
+    const selectedGyms = filteredGyms.filter((gym) =>
+      gym.schedules.some((schedule) => schedule.hour === selectTurn),
+    )
+
+    return selectedGyms
+  } catch (err) {
+    console.error('ERRO!', err)
+    throw err
+  }
+}
